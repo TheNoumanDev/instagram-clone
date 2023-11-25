ad3be74b62d275f413e721218f5750bda9bd6f0f
TheNoumanDev <m_nouman@hotmail.com>
2023-11-25 21:28:38 +0500

    Addes the feed screen

--- instagram_clone/lib/utils/global_constants.dart
+++ instagram_clone/lib/utils/global_constants.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/screens/add_post_screen.dart';
+import 'package:instagram_clone/screens/feed.dart';
 
 const webScreenSize = 600;
 
 const HomeScreens = [
-  Text("Home"),
+  FeedScreen(),
   Text("Search"),
   AddPostScreen(),
   Text("Notification"),
---
+++ instagram_clone/lib/widgets/post_card.dart
@@ -0,0 +1,200 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/utils/colors.dart';
+import 'package:intl/intl.dart';
+
+class PostCard extends StatelessWidget {
+  final snap;
+  const PostCard({
+    super.key,
+    required this.snap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: mobileBackgroundColor,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      child: Column(
+        children: [
+          // Header Section
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16)
+                .copyWith(right: 0),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 16,
+                  backgroundImage: NetworkImage(snap["profImage"]),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          snap["username"],
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                IconButton(
+                    onPressed: () {
+                      showDialog(
+                        context: context,
+                        builder: (context) => Dialog(
+                          child: ListView(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 10,
+                                horizontal: 10,
+                              ),
+                              shrinkWrap: true,
+                              children: [
+                                'Delete',
+                                'Edit',
+                              ]
+                                  .map(
+                                    (e) => InkWell(
+                                      onTap: () {},
+                                      child: Container(
+                                        padding: const EdgeInsets.symmetric(
+                                            vertical: 12, horizontal: 16),
+                                        child: Text(e),
+                                      ),
+                                    ),
+                                  )
+                                  .toList()),
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.more_vert))
+              ],
+            ),
+          ),
+
+          // Image Area
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.35,
+            width: double.infinity,
+            child: Image.network(
+              snap["postUrl"],
+              fit: BoxFit.cover,
+            ),
+          ),
+
+          // Like Commnet section
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.comment_outlined,
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.send,
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.bookmark_outline,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          // for description
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '${snap['likes'].length} likes',
+                  style: const TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.only(
+                    top: 8,
+                  ),
+                  child: RichText(
+                    text: TextSpan(
+                      style: const TextStyle(color: primaryColor),
+                      children: [
+                        TextSpan(
+                          text: snap['username'],
+                          style: const TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        const TextSpan(
+                          text: "  ",
+                        ),
+                        TextSpan(
+                          text: snap['description'],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 4),
+                        child: const Text(
+                          "View all 200 comments",
+                          style: TextStyle(
+                            color: secondaryColor,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 4),
+                          child: Text(
+                            DateFormat.yMMMd().format(
+                              snap['datePublished'].toDate(),
+                            ),
+                            style: const TextStyle(
+                              color: secondaryColor,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
